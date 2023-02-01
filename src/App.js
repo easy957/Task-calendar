0da0eda1a984ee393c0c707982303de215240f20
easy957 <easy957@gmail.com>
2023-02-01 21:24:34 +0200

    something fixed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import CalendarJs from "./calendar/calendar";
 
 function App() {
+  const calendar = CalendarJs().of(2023, 0);
+  console.log(calendar);
+
+  // calendar.calendar = calendar.calendar.map((week) => {
+  //   const newWeek = week.slice(1);
+  //   newWeek.push(week[0]);
+  //   return newWeek;
+  // });
+
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="container">
+        <div>
+          {calendar.weekdaysAbbr.map((weekday) => (
+            <span className="day">{weekday}</span>
+          ))}
+        </div>
+        {calendar.calendar.map((week) => (
+          <div>
+            {week.map((day) => (
+              <span
+                className="day"
+                onClick={() => {
+                  console.log(day);
+                }}
+              >
+                {day}
+              </span>
+            ))}
+          </div>
+        ))}
+      </div>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +47,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
